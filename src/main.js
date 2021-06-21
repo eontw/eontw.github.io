4fff7a9e4188b185518648865f8e8081fa160cb0
wts <819210987>
2021-06-21 22:38:33 +0800

    解决"export 'createWebHashHistory' was not found in 'vue-router'
    npm install vue-router@next --save

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
+// import './plugins/element.js'
 
 const app= createApp(App).use(router)
 app.mount('#app')
