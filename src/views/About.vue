cfb79be61e7cfe53955c3eb478715ea29be1a569
wts <819210987>
2021-06-22 10:14:09 +0800

    新增axios

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,34 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  props: {},
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {
+    axios
+      .get("/json/test.json")
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+</style>
+
+
