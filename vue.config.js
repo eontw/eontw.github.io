ca4840bcffe2869e659f0b382b9449966203d0de
wts <819210987>
2021-06-22 13:53:03 +0800

    新增element plus依赖

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
         open: true,
         port: 8080
     },
-    publicPath : process.env.NODE_ENV === 'production'?'/docs':'./',//本地打包build测试时/docs  部署pages时./
+    publicPath : process.env.NODE_ENV === 'production'?'/dist':'./',//本地打包build测试时/docs  部署pages时./
     //构建时的输出目录
-    outputDir : 'docs',
+    outputDir : process.env.NODE_ENV === 'production'?'dist':'docs',
 }
